ea707410359ce093e1fbb4ef93db3dac27315beb
Abbas W <abbas.code786@gmail.com>
2025-05-12 20:34:51 +0530

    bugs fixed

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error.ejs", { err });
 });
 
-// starting the server
+// starting the server.
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
